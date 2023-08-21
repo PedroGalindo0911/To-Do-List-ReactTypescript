cd692ac4789afcc950c410f055ff1ac7536c9d10
Pedro Galindo <pedrogalindo0911@gmail.com>
2023-08-21 16:19:40 -0600

    Crear componentes
    Se crearon y probaron los componentes que se muestran en la pagina principal

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,8 @@
+import './Copyright.css'
+
+export const Copyright: React.FC = () => (
+  <footer className='copyright'>
+    <h4>Curso de React desde cero ⚛️ － <span>@midudev</span></h4>
+    <h5>Creando un TO-DO-LIST con TS</h5>
+  </footer>
+)
